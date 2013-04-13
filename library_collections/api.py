529048da2acb2437718961bc850f21b7e09e3d5b
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-04-12 21:18:44 -0700

    make API authentication & authorization explicit

--- library_collections/api.py
+++ library_collections/api.py
@@ -1,6 +1,10 @@
 from tastypie.resources import ModelResource
+from tastypie.authentication import Authentication
+from tastypie.authorization import ReadOnlyAuthorization
 from library_collections.models import ProvenancialCollection
 
 class ProvenancialCollectionResource(ModelResource):
     class Meta:
         queryset = ProvenancialCollection.objects.all()
+        authentication = Authentication()
+        authorization = ReadOnlyAuthorization()
