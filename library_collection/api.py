35593cbc7436778d2514af609654d76569af6b3a
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-04-13 21:45:15 -0700

    pull the campus into the provenancial collection

--- library_collection/api.py
+++ library_collection/api.py
@@ -1,10 +1,21 @@
+from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.authentication import Authentication
 from tastypie.authorization import ReadOnlyAuthorization
-from library_collection.models import ProvenancialCollection
+from library_collection.models import ProvenancialCollection, Campus
+
+class CampusResource(ModelResource):
+    class Meta:
+        queryset = Campus.objects.all()
+        authentication = Authentication()
+        authorization = ReadOnlyAuthorization()
+        excludes = ['id']
 
 class ProvenancialCollectionResource(ModelResource):
+    campus = fields.ToManyField(CampusResource, 'campus', full=True)
+
     class Meta:
         queryset = ProvenancialCollection.objects.all()
         authentication = Authentication()
         authorization = ReadOnlyAuthorization()
+        excludes = ['id']
