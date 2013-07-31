ce86811d9820718154bbb2cc30de48a596637213
Brian Tingle <brian.tingle@ucop.edu>
2013-07-30 18:32:03 -0700

    going south

--- collection_registry/settings.py
+++ collection_registry/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/apps/registry/code/avram/test-db',    # Or path to database file if using sqlite3.
+        'NAME': 'test-db',    # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
     'django.contrib.sitemaps',
     # Uncomment the next line to enable the admin:
     'admin_bootstrap',
+    'south',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
@@ -182,7 +183,7 @@ ADMIN_REORDER = (
 )
 
 
-try: 
-    from local_settings import *; 
-except ImportError: 
+try:
+    from local_settings import *
+except ImportError:
     pass
