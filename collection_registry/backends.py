d6c2ba510415e5abcf677464461c983cc2ae0873
Brian Tingle <brian.tingle@ucop.edu>
2013-07-30 19:43:58 -0700

    add user to group and try to send an email

--- collection_registry/backends.py
+++ collection_registry/backends.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.backends import RemoteUserBackend
 import os
 import collection_registry.middleware
+from django.contrib.auth.models import Group
+from django.core.mail import send_mail
 
 class RegistryUserBackend(RemoteUserBackend):
     def configure_user(self, user):
@@ -11,4 +13,14 @@ class RegistryUserBackend(RemoteUserBackend):
         request = collection_registry.middleware.get_current_request()
         user.email = request.META['mail']
         user.save()
+        # http://stackoverflow.com/a/6288863/1763984
+        g = Group.objects.get(name=request.META['Shib-Identity-Provider']) 
+        g.user_set.add(user)
+
+        # https://docs.djangoproject.com/en/dev/topics/email/
+        send_mail('New registry user {0}'.format(user.username), 'New registry user\n\teppn: {0}\n\temail:{1}'.format(user.username, user.email), user.email,
+            ['oacops@cdlib.org'], fail_silently=False)
+
         return user
+
+
