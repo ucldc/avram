21619c2f6e053900f31beb7e065e100e7cdbb817
Mark Redar <mredar@gmail.com>
2013-10-11 12:13:52 -0700

    HttpAuth mock for dev & testing
    Added a middleware that mocks Http Basic Auth, setting the
    request.META['REMOTE_USER'] to whatever is entered in the basic auth
    dialog.
    For testing, you must add a HTTP_AUTHENTICATION parameter to all testing
    client requests that you need an authenticated user.
    This does not yet work for the "first" login of a user, as the
    RegistryUserBackend will look for the request.META keys 'mail' and
    'Shib-Identity-Provider' to modify the new user. You will have to do
    this manually for now.

---
+++ collection_registry/middleware/remoteuser_mock.py
@@ -0,0 +1,46 @@
+from django.conf import settings
+
+try:
+    username = settings.REMOTE_USER_MOCK_USERNAME
+except AttributeError:
+    username  = 'test_superuser'
+try:
+    paths_locked = settings.REMOTE_USER_MOCK_PATHS
+except AttributeError:
+    paths_locked = ['/edit', '/admin']
+
+class RemoteUserMockMiddleware(object):
+    '''Mock a remote user, maybe want to prompt user.
+    '''
+    def process_request(self, request):
+        for path in paths_locked:
+            if request.path.startswith(path):
+                request.META['REMOTE_USER'] = username
+                return None
+
+from django.http import HttpResponse
+#http://djangosnippets.org/snippets/2468/
+class BasicAuthMockMiddleware(object):
+    def unauthed(self):
+        response = HttpResponse("""<html><title>Auth required</title><body>
+                                <h1>Authorization Required</h1></body></html>""", mimetype="text/html")
+        response['WWW-Authenticate'] = 'Basic realm="Development"'
+        response.status_code = 401
+        return response
+
+    def process_request(self,request):
+        for path in paths_locked:
+            if request.path.startswith(path):
+                if not request.META.has_key('HTTP_AUTHORIZATION'):
+                    return self.unauthed()
+                else:
+                    authentication = request.META['HTTP_AUTHORIZATION']
+                    (authmeth, auth) = authentication.split(' ',1)
+                    if 'basic' != authmeth.lower():
+                        return self.unauthed()
+                    auth = auth.strip().decode('base64')
+                    username, password = auth.split(':',1)
+                    ## let anything through and set REMOTE_USER
+                    request.META['REMOTE_USER'] = username
+                    return None
+        return None
