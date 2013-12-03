d67b66eb36339d111bb3e6ad195fbd5772ef66b6
Mark Redar <mredar@gmail.com>
2013-12-03 10:56:38 -0800

    Change so test-app.bash accepts up to 10 params.
    Allows changing of settings and such.

--- test-app.bash
+++ test-app.bash
@@ -2,4 +2,4 @@
 
 # Just a little reminder to use the collection_registyr.test_settings.py file
 
-python manage.py test --settings=collection_registry.test_settings $1
+python manage.py test --settings=collection_registry.test_settings ${@:1:10}
