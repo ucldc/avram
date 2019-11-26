8c7db0ecf05d41fb480d9a098a141b4ded5c6eb2
amywieliczka <amywieliczka@gmail.com>
2019-11-25 16:23:38 -0800

    Moved exhibits from avram to exhibitapp

--- exhibits/apps.py
+++
@@ -1,7 +0,0 @@
-
-
-from django.apps import AppConfig
-
-
-class ExhibitsConfig(AppConfig):
-    name = 'exhibits'
--- exhibits/teacher_urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.lessonPlanDirectory, name='lessonPlanDirectory'),    
-    url(r'^(?P<lesson_id>\d+)/(?P<lesson_slug>[-\w]+)/$', views.lessonPlanView, name='lessonPlanView'),
-    url(r'^(?P<lesson_id>\d+)/items/(?P<item_id>.+)/$', views.lessonPlanItemView, name='lessonPlanItemView'),
-]
