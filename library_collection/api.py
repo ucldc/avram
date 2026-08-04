7809b894b1337e5003721331eab881a6a6282635
Amy Wieliczka <amywieliczka@gmail.com>
2026-08-04 15:22:43 -0400

    Allow logged-in users to access api

--- library_collection/api.py
+++ library_collection/api.py
@@ -1,17 +1,22 @@
 from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.serializers import Serializer
-from tastypie.authentication import ApiKeyAuthentication as Authentication
+from tastypie.authentication import ApiKeyAuthentication, SessionAuthentication, MultiAuthentication
 from tastypie.authorization import ReadOnlyAuthorization
 from library_collection.models import Collection, Campus, Repository
 from library_collection.models import CollectionCustomFacet
 from tastypie.constants import ALL
 from tastypie.api import Api
 
+default_authentication = MultiAuthentication(
+    ApiKeyAuthentication(),
+    SessionAuthentication()
+)
+
 class CampusResource(ModelResource):
     class Meta:
         queryset = Campus.objects.all()
-        authentication = Authentication()
+        authentication = default_authentication
         authorization = ReadOnlyAuthorization()
 
 
@@ -19,7 +24,7 @@ class RepositoryResource(ModelResource):
     campus = fields.ToManyField(CampusResource, 'campus', full=True)
     class Meta:
         queryset = Repository.objects.all()
-        authentication = Authentication()
+        authentication = default_authentication
         authorization = ReadOnlyAuthorization()
 
 
@@ -31,7 +36,7 @@ class CollectionResource(ModelResource):
 
     class Meta:
         queryset = Collection.objects.all()
-        authentication = Authentication()
+        authentication = default_authentication
         authorization = ReadOnlyAuthorization()
         serializer = Serializer(formats=['json', 'jsonp', 'xml', 'yaml', 'plist'])
         filtering = {
@@ -76,7 +81,7 @@ class RikoltiCollectionResource(CollectionResource):
 
     class Meta:
         queryset = Collection.objects.all()
-        authentication = Authentication()
+        authentication = default_authentication
         list_allowed_methods = ['get']
         filtering = rikolti_filters
         excludes = rikolti_excludes
@@ -101,7 +106,7 @@ class RikoltiMapperResource(RikoltiCollectionResource):
 class RikoltiFetcherResource(RikoltiCollectionResource):
     class Meta:
         queryset = Collection.objects.all()
-        authentication = Authentication()
+        authentication = default_authentication
         list_allowed_methods = ['get']
         filtering = rikolti_filters
         excludes = (
@@ -116,7 +121,7 @@ class CustomFacetResource(ModelResource):
 
     class Meta:
         queryset = CollectionCustomFacet.objects.all()
-        authentication = Authentication()
+        authentication = default_authentication
         authorization = ReadOnlyAuthorization()
         resource_name = 'custom_facet'
 
